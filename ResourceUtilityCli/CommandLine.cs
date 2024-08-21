267b6d9fac60867070d509e3b0a7211086dc7a25
Kiran Welle <me@kiranwelle.com>
2024-08-20 17:01:31 -0700

    Add initial command line options

--- ResourceUtilityCli/CommandLine.cs
+++ ResourceUtilityCli/CommandLine.cs
@@ -1,14 +1,107 @@
-﻿using ResourceUtilityLib;
+﻿using System.Runtime.CompilerServices;
 
-foreach (var arg in args)
+using ResourceUtilityLib;
+
+enum Operations {
+    NoOp,
+    OpAdd,
+    OpRemove,
+    OpExtract,
+    OpList,
+    OpResponseFile,
+    OpVerify
+}
+
+class ResourceUtilityCli
 {
-    Console.WriteLine("Processing " + arg);
-    ResourceUtility ru = new ResourceUtility(arg);
-
-    Console.WriteLine("Version: " + ru.FileVersion());
-    Console.WriteLine("Resources: " + ru.Count());
-    foreach (string str in ru.ListContents(true))
+    static void Main(string[] args)
     {
-        Console.WriteLine(str);
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Output help info here.");
+            return;
+        }
+
+        string resfile = args[0];
+
+        Console.WriteLine("Processing " + resfile);
+        ResourceUtility ru = new ResourceUtility(resfile);
+
+        Console.WriteLine("Version: " + ru.FileVersion());
+        Console.WriteLine("Resources: " + ru.Count());
+
+        Operations operation = Operations.NoOp;
+        string filename = "";
+        for (int i = 1; i < args.Length; i++)
+        {
+            bool file_operation = false;
+
+            switch (Char.ToUpper(args[i][0]))
+            {
+                case 'E':
+                    operation = Operations.OpExtract;
+                    file_operation = true;
+                    break;
+                case 'H':
+                    switch(Char.ToUpper(args[i][1]))
+                    {
+                        case 'C':
+                            ru.useCRCHash();
+                            break;
+                        case 'I':
+                            ru.useIDHash();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid hashing algorithm.");
+                            break;
+                    }
+                    break;
+                case 'L':
+                    operation = Operations.OpList;
+                    break;
+                case 'V':
+                    operation = Operations.OpVerify;
+                    break;
+            }
+
+            if( file_operation )
+            {
+                filename = args[++i];
+            }
+
+            switch(operation)
+            {
+                case Operations.OpExtract:
+                    Extract(ru, filename);
+                    break;
+                case Operations.OpList:
+                    List(ru);
+                    break;
+                case Operations.OpVerify:
+                    List(ru, true);
+                    break;
+            }
+        }
+    }
+
+    static void List(ResourceUtility ru, bool verify = false)
+    {
+        foreach (string str in ru.ListContents(verify))
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    static void Extract(ResourceUtility ru, string filename)
+    {
+        ru.useCRCHash();
+        try
+        {
+            ru.ExtractFile(filename);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine(String.Format("File {0} was not found in the resource file.", filename));
+        }
     }
 }
