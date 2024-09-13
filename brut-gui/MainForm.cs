dd35c50bfce36684840c95d4df3925f215fa7a28
Kiran Welle <me@kiranwelle.com>
2024-09-13 10:44:36 -0700

    Initial Eto Forms test

---
+++ brut-gui/MainForm.cs
@@ -0,0 +1,14 @@
+using Eto.Drawing;
+using Eto.Forms;
+using System;
+
+namespace BrutGui
+{
+    public partial class MainForm : Eto.Forms.Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ brut-gui/MainForm.eto.cs
@@ -0,0 +1,14 @@
+
+namespace BrutGui
+{
+   partial class MainForm : Eto.Forms.Form
+   {
+      void InitializeComponent()
+      {
+		// sets the client (inner) size of the window for your content
+		this.ClientSize = new Eto.Drawing.Size(600, 400);
+
+		this.Title = "Birthright Resource Utility";
+      }
+   }
+}
