95d6f7884c53a29c8602f68551e60bd269893be7
Kiran Welle <me@kiranwelle.com>
2025-08-28 09:13:30 -0700

    Test: Add tests for LZSS decompression and PCX to bitmap conversion

---
+++ brut-test/ImageHandlerTest.cs
@@ -0,0 +1,20 @@
+﻿using ResourceUtilityLib;
+
+namespace BrutTest
+{
+    [TestClass]
+    public class ImageHandlerTest
+    {
+        public TestContext TestContext { get; set; }  // instance property
+
+        [TestMethod]
+        public void ConvertPCXtoBitmapWithoutRotation()
+        {
+            byte[] pcx = File.ReadAllBytes(@"TestData\test.pcx");
+            byte[] expected = File.ReadAllBytes(@"TestData\test_uncomp.bin");
+            byte[] actual = ImageHandler.ConvertPCXToBitmap(pcx, false);
+            
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ brut-test/LZSSTest.cs
@@ -0,0 +1,31 @@
+﻿using ResourceUtilityLib;
+
+namespace BrutTest
+{
+    [TestClass]
+    public class LZSSTest
+    {
+        public TestContext TestContext { get; set; }  // instance property
+
+        [TestMethod]
+        [Ignore("Not implemented")]
+        public void LZSSEncode()
+        {
+            byte[] uncompressed = File.ReadAllBytes(@"TestData\test_uncomp.bin");
+            byte[] expected = File.ReadAllBytes(@"TestData\test_comp.bin");
+            byte[] actual = LZSS.Encode(uncompressed);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LZSSDecode()
+        {
+            byte[] compressed = File.ReadAllBytes(@"TestData\test_comp.bin");
+            byte[] expected = File.ReadAllBytes(@"TestData\test_uncomp.bin");
+            byte[] actual = LZSS.Decode(compressed, (uint)expected.Length);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
